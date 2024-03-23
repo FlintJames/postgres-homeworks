82aa6dc23ca1831f48c6fc7cb49be2bf32627231
FlintJames <lyuciana@yandex.ru>
2024-03-23 04:26:15 +0300

    change main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -2,22 +2,53 @@
 import csv
 import psycopg2
 
-with psycopg2.connect(
-        host="localhost",
-        database="north",
-        user="postgres",
-        password="12345"
-) as connection:
+conn_params = dict(host="localhost", database="north", user="postgres", password="1940")
 
-    with connection.cursor() as cursor:
-        with open('north_data\\employees_data.csv') as csv_file:
-            header = next(csv_file)
-            csv_reader = csv.reader(csv_file)
-            for row in csv_reader:
+with psycopg2.connect(**conn_params) as conn:
+    with conn.cursor() as cur:
+        with open('north_data/employees_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                cur.execute("TRUNCATE emploees CASCADE")
+                cur.execute("INSERT INTO emploees (employee_id, first_name, last_name, title, birth_date, notes)"
+                            "VALUES (%s, %s, %s, %s, %s, %s)", row)
+                cur.execute("SELECT * FROM emploees")
 
-                query = """INSERT INTO employees 
-                VALUES (%s, %s, %s, %s, %s, %s)"""
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+conn.close()
 
-                cursor.execute(query, row)
+with psycopg2.connect(**conn_params) as conn:
+    with conn.cursor() as cur:
+        with open('north_data/customers_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                cur.execute("TRUNCATE customers CASCADE")
+                cur.execute("INSERT INTO customers (customer_id, company_name, contact_name)"
+                            "VALUES (%s, %s, %s)", row)
+                cur.execute("SELECT * FROM customers")
 
-connection.close()
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+conn.close()
+
+try:
+    with psycopg2.connect(**conn_params) as conn:
+        with conn.cursor() as cur:
+            with open('north_data/orders_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cur.execute("INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city)"
+                                "VALUES (%s, %s, %s, %s, %s)", row)
+                    cur.execute("SELECT * FROM orders")
+
+            row = cur.fetchall()
+            for row in rows:
+                print(row)
+finally:
+    conn.close()
