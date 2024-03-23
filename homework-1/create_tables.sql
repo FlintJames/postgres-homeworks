a349e99298d41dae59fe9a2dc71a04ab464173bb
FlintJames <lyuciana@yandex.ru>
2024-03-23 03:31:06 +0300

    change create_tables.sql change main.py

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE emploees
+(
+    employee_id int PRIMARY KEY,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(50) NOT NULL,
+	title varchar(100) NOT NULL,
+	bird_date date NOT NULL,
+	notes text NOT NULL
+);
+
+
+CREATE TABLE customers
+(
+    customer_id varchar(5) PRIMARY KEY,
+    company_name varchar(100) NOT NULL,
+    contact_name varchar(50) NOT NULL
+);
+
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+    customer_id varchar(5) REFERENCES customers(customer_id) NOT NULL,
+    employee_id int REFERENCES emploees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,23 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+with psycopg2.connect(
+        host="localhost",
+        database="north",
+        user="postgres",
+        password="12345"
+) as connection:
+
+    with connection.cursor() as cursor:
+        with open('north_data\\employees_data.csv') as csv_file:
+            header = next(csv_file)
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+
+                query = """INSERT INTO employees 
+                VALUES (%s, %s, %s, %s, %s, %s)"""
+
+                cursor.execute(query, row)
+
+connection.close()
